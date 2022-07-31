9003068d9591078b6ed8ef63061bf2b482f9ebbb
achingbrain <alex@achingbrain.net>
2022-07-31 17:06:45 +0100

    deps: update uint8arraylist and protons deps

--- packages/libp2p-daemon-server/src/pubsub.ts
+++ packages/libp2p-daemon-server/src/pubsub.ts
@@ -57,7 +57,7 @@ export class PubSubOperations {
           topicIDs: [msg.topic],
           signature: msg.signature,
           key: msg.key
-        }))
+        }).subarray())
       })
 
       yield OkResponse()
