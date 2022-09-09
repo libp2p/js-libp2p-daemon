dae43259f74c8ee656f1b707583a712047b5bc8a
saul <saul@organicdesign.nz>
2022-09-09 16:26:32 +1200

    Add ipfs boostrap peers as default.

--- packages/libp2p-daemon/src/index.ts
+++ packages/libp2p-daemon/src/index.ts
@@ -52,7 +52,7 @@ export default async function main (processArgs: string[]) {
     .option('bootstrapPeers', {
       desc: 'Comma separated list of bootstrap peers; defaults to the IPFS DHT peers',
       type: 'string',
-      default: ''
+      default: '/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ,/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN,/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb,/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp,/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa,/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt'
     })
     .option('dht', {
       desc: 'Enables the DHT in full node mode',
