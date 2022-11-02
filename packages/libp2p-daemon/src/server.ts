a16d9d7b5d0888448d415a33723476934adad3e3
saul <saul@organicdesign.nz>
2022-11-02 13:04:07 +1300

    Migrate server to libp2p 0.40.

--- packages/libp2p-daemon/src/server.ts
+++ packages/libp2p-daemon/src/server.ts
@@ -2,18 +2,18 @@ import { promises as fs } from 'fs'
 import type { Multiaddr } from '@multiformats/multiaddr'
 import { createServer, Libp2pServer } from '@libp2p/daemon-server'
 import { Libp2p, createLibp2p, Libp2pOptions } from 'libp2p'
-import { PreSharedKeyConnectionProtector } from 'libp2p/pnet'
-import { Noise } from '@chainsafe/libp2p-noise'
-import { Mplex } from '@libp2p/mplex'
-import { TCP } from '@libp2p/tcp'
-import { WebSockets } from '@libp2p/websockets'
-import { Bootstrap } from '@libp2p/bootstrap'
-import { GossipSub } from '@chainsafe/libp2p-gossipsub'
-import { FloodSub } from '@libp2p/floodsub'
-import { PubSubPeerDiscovery } from '@libp2p/pubsub-peer-discovery'
+import { preSharedKey } from 'libp2p/pnet'
+import { noise } from '@chainsafe/libp2p-noise'
+import { mplex } from '@libp2p/mplex'
+import { tcp } from '@libp2p/tcp'
+import { webSockets } from '@libp2p/websockets'
+import { bootstrap } from '@libp2p/bootstrap'
+import { gossipsub } from '@chainsafe/libp2p-gossipsub'
+import { floodsub } from '@libp2p/floodsub'
+import { pubsubPeerDiscovery } from '@libp2p/pubsub-peer-discovery'
 import { unmarshalPrivateKey } from '@libp2p/crypto/keys'
 import { createFromPrivKey } from '@libp2p/peer-id-factory'
-import { KadDHT } from '@libp2p/kad-dht'
+import { kadDHT } from '@libp2p/kad-dht'
 
 export default {
   createLibp2pServer: async function (listenAddr: Multiaddr, argv: any): Promise<Libp2pServer> {
@@ -24,9 +24,9 @@ export default {
         announce: argv.announceAddrs.split(',')
       },
 
-      transports: [new TCP(), new WebSockets()],
-      connectionEncryption: [new Noise()],
-      streamMuxers: [new Mplex()],
+      transports: [tcp(), webSockets()],
+      connectionEncryption: [noise()],
+      streamMuxers: [mplex()],
       peerDiscovery: []
     }
 
@@ -42,8 +42,8 @@ export default {
     // Enable bootstrap peers.
     if (argv.bootstrap === true && options.peerDiscovery != null) {
       options.peerDiscovery.push(
-        new Bootstrap({
-          interval: argv.discoveryInterval,
+        bootstrap({
+          timeout: argv.discoveryInterval,
           list: argv.bootstrapPeers.split(',')
         })
       )
@@ -54,10 +54,10 @@ export default {
       // Router implementation.
       switch (argv.pubsubRouter) {
         case 'gossipsub':
-          options.pubsub = new GossipSub({ allowPublishToZeroPeers: true })
+          options.pubsub = gossipsub({ allowPublishToZeroPeers: true })
           break
         case 'floodsub':
-          options.pubsub = new FloodSub()
+          options.pubsub = floodsub()
           break
         default:
           throw new Error('invalid pubsubRouter type')
@@ -65,22 +65,24 @@ export default {
 
       // Peer discovery
       if (argv.pubsubDiscovery === true && options.peerDiscovery != null) {
-        options.peerDiscovery.push(
-          new PubSubPeerDiscovery({ interval: argv.discoveryInterval })
-        )
+        const discovery = pubsubPeerDiscovery({ interval: argv.discoveryInterval })
+
+        // @libp2p/pubsub-peer-discovery at version 7.0.0 seems to have type compatibility problems.
+        // @ts-expect-error
+        options.peerDiscovery.push(discovery)
       }
     }
 
     // Enable DHT
     if (argv.dht === true) {
-      options.dht = new KadDHT()
+      options.dht = kadDHT()
     }
 
     // Configure PSK
     if (argv.psk != null) {
       const swarmKey: Buffer = await fs.readFile(argv.psk)
 
-      options.connectionProtector = new PreSharedKeyConnectionProtector({
+      options.connectionProtector = preSharedKey({
         psk: swarmKey
       })
     }
